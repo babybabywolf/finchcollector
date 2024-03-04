9cec19133a49acad54eb1981270d249208634036
Amber <windjiayu@gmail.com>
2024-03-04 21:59:21 +1100

    create all finches page

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+ path('about/', views.about, name='about'), # `name='home'` kwarg gives the route a name - naming is optional, but good practice (it will come in handy later)
+ path('finches/', views.finch_index, name='index'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
+
+finches = [
+  {'name': 'Peep', 'species': 'Zebra Finch', 'description': 'vibrant and energetic', 'age': 1},
+  {'name': 'Sunny', 'species': 'American Goldfinch', 'description': 'bright yellow and cheerful', 'age': 2},
+  {'name': 'Glimmer', 'species': 'Gouldian Finch', 'description': 'colorful and shy', 'age': 3},
+  {'name': 'Chirpy', 'species': 'House Finch', 'description': 'social and vocal', 'age': 1},
+]
+
 # Create your views here.
+def about (request):
+    return render(request, 'about.html')
+
+
+def finch_index (request):
+    return render(request, 'pinches/index.html', {'finches': finches})
+               
