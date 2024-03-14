ff52767a39689b514a760e457f6a56a1ffa9130d
Amber <windjiayu@gmail.com>
2024-03-14 20:52:51 +1100

    complete part4

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
  path('about/', views.about, name='about'), # `name='home'` kwarg gives the route a name - naming is optional, but good practice (it will come in handy later)
  path('finches/', views.finch_index, name='index'),
  path('finches/<int:finch_id>/', views.finch_detail, name='detail'),
+ path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name="add_feeding"),
  path('finches/create/', views.FinchCreate.as_view(), name="finch_create"),
  path('finches/<int:pk>/update', views.FinchUpdate.as_view(), name="finch_update"),
  path('finches/<int:pk>/delete', views.FinchDelete.as_view(), name="finch_delete"),
+ 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from .models import Finch, Feeding
+from .forms import FeedingForm
 
 
 
@@ -19,8 +20,11 @@ def finch_index (request):
                
 def finch_detail (request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
-        'finch':finch
+        'finch':finch,
+        'feeding_form':feeding_form,
+
     }
                   )
 
@@ -37,3 +41,16 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
     model = Finch
     success_url = "/finches"
+
+
+def add_feeding (request, finch_id):
+    ## create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  ## validate the form
+  if form.is_valid():
+    ## don't save the form to the db until it
+   
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
